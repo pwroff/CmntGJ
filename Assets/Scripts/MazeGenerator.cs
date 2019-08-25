739c445c764b9e4b36d7fb72275c863fda6a4d64
Leonid Lazaryev <leonidlazaryev@gmail.com>
2019-08-25 19:06:34 +0200

    implemented basic maze generator

---
+++ Assets/Scripts/MazeGenerator.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Threading;
+
+public class MazeGenerator : MonoBehaviour
+{
+    public Texture2D source;
+    public float tileDimension = 1f;
+    public Transform parent;
+
+
+    [SerializeField, HideInInspector]
+    MazeNode[,] tiles;
+    
+    void ClearPrev()
+    {
+        for (int i = parent.childCount; i > 0; i--)
+        {
+            DestroyImmediate(parent.GetChild(i - 1).gameObject);
+        }
+    }
+
+    [InspectorButton("CreateMaze")]
+    public string generate;
+
+    MazeNode GetNeighbor(int x, int y)
+    {
+        MazeNode neighbor = null;
+
+        if (x > -1 && x < tiles.GetLength(0) && y > -1 && y < tiles.GetLength(1))
+        {
+            neighbor = tiles[x, y];
+        }
+
+        return neighbor;
+    }
+
+    void CreateMaze()
+    {
+        ClearPrev();
+        tiles = new MazeNode[source.width, source.height];
+        var pixels = source.GetPixels();
+        int width = source.width;
+        int height = source.height;
+        
+
+        Action<int> initialMaze = (int start) =>
+        {
+            for (int y = start; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (pixels[y * width + x].r > 0)
+                    {
+                        var config = new MazeNode.MazeConfiguration()
+                        {
+                            tilesParent = parent,
+                            tileDimension = tileDimension,
+                            gridPosition = new Vector2Int(x, y)
+                        };
+                        tiles[x, y] = new MazeNode(config);
+                    }
+
+                }
+            }
+        };
+
+        Action<int> setNighborsMaze = (int start) =>
+        {
+            for (int y = start; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (tiles[x, y] != null)
+                    {
+                        tiles[x, y].SetNeighbor(0, GetNeighbor(x, y + 1));
+                        tiles[x, y].SetNeighbor(1, GetNeighbor(x + 1, y));
+                        tiles[x, y].SetNeighbor(2, GetNeighbor(x, y - 1));
+                        tiles[x, y].SetNeighbor(3, GetNeighbor(x - 1, y));
+                    }
+
+                }
+            }
+        };
+
+        int readyThreads = 0;
+        int maxThreads = 8;
+        int itemsPerThread = height / maxThreads;
+        int reminder = (height) - (itemsPerThread * (maxThreads - 1));
+
+        Action<Action<int>> enableRun = (a) =>
+        {
+            readyThreads = 0;
+            for (int i = 0; i < maxThreads - 1; i++)
+            {
+                int c = i;
+                var t = new Thread(() =>
+                {
+                    a(i * itemsPerThread);
+                    readyThreads++;
+                });
+                t.Start();
+            }
+            a(reminder);
+            readyThreads++;
+            while (readyThreads < maxThreads)
+            {
+                Thread.Sleep(50);
+                Debug.Log("Processing...");
+            }
+        };
+
+        enableRun(initialMaze);
+        enableRun(setNighborsMaze);
+
+        for (int y = 0; y < source.height; y++)
+        {
+            for (int x = 0; x < source.width; x++)
+            {
+                if (tiles[x, y] != null)
+                    tiles[x, y].BuildTile();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MazeNode.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+[Serializable]
+public class MazeNode
+{
+
+    public struct MazeConfiguration
+    {
+        public Transform tilesParent;
+        public GameObject floorPrefab;
+        public GameObject wallPrefab;
+        public Vector2Int gridPosition;
+        public float tileDimension;
+    }
+
+    [SerializeField, HideInInspector]
+    MazeNode[] neighbors = new MazeNode[4];
+
+    MazeConfiguration configuration;
+    GameObject gameObject;
+
+    public MazeNode(MazeConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    void EnsureAndThrow(ref int direction)
+    {
+        if (direction < 0 || direction > 4)
+            throw new System.Exception("There are only 4 available directions in our world");
+    }
+
+    /// <summary>
+    /// directions:
+    /// 0: north/forward
+    /// 1: east/right
+    /// 2: south/back
+    /// 3: west/left
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <param name="node"></param>
+    public void SetNeighbor(int direction, MazeNode node)
+    {
+        EnsureAndThrow(ref direction);
+        neighbors[direction] = node;
+    }
+
+    public void BuildTile()
+    {
+        gameObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        gameObject.name = string.Format("Floor-{0}-{1}", configuration.gridPosition.x, configuration.gridPosition.y);
+        gameObject.transform.Rotate(new Vector3(90, 0, 0));
+        gameObject.transform.parent = configuration.tilesParent;
+        gameObject.transform.localScale = Vector3.one * configuration.tileDimension;
+        gameObject.transform.localPosition = new Vector3(configuration.gridPosition.x * configuration.tileDimension, 0, configuration.gridPosition.y * configuration.tileDimension);
+        for (int i = 0; i < 4; i++)
+        {
+            if (neighbors[i] == null)
+            {
+                var wall =  GameObject.CreatePrimitive(PrimitiveType.Quad);
+                wall.name = "wall" + i;
+                wall.transform.parent = gameObject.transform;
+                wall.transform.localScale = Vector3.one;
+                wall.transform.rotation = Quaternion.identity;
+                wall.transform.position = gameObject.transform.position + configuration.tileDimension * GetLocalPositionForOrientation(i);
+                wall.transform.Rotate(new Vector3(0, i * 90, 0));
+            }
+        }
+    }
+
+    private Vector3 GetLocalPositionForOrientation(int i)
+    {
+        EnsureAndThrow(ref i);
+        switch (i)
+        {
+            case 0:
+                return new Vector3(0, 0.5f, 0.5f);
+            case 1:
+                return new Vector3(0.5f, 0.5f, 0);
+            case 2:
+                return new Vector3(0, 0.5f, -0.5f);
+            case 3:
+                return new Vector3(-0.5f, 0.5f, 0);
+
+            default:
+                return default;
+
+        }
+    }
+}
